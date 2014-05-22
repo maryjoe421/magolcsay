3049709d07e181e5be5cc3676315a2d659f9a8f8
Mario Leszko <mario.leszko@shiwaforce.com>
2014-05-22 20:11:38 +0200

    fixing + picture list with delete added

--- admin/file.php
+++ admin/file.php
@@ -1,9 +1,13 @@
 <div class="text-content">
 <?php
 if($_SESSION["privilege"] == "user") {
+	if(isset($_GET['pic'])) {
+		unlink('../picture/' . $_GET['pic']);
+		header("location: index.php?b=file");
+	}
 ?>
 	<h1>Fájlok feltöltése</h1>
-	<p>A feltöltendő fájlokat hozd a következő formába <b>more_than_one_word.mp3, tukor_meg_lira.png, stb.</b>, vagyis az elnevezés során ügyelj arra, hogy <b>szóköz</b> illetve <b>ékezetes- és speciális karakter</b> (&amp;, @, #, &lt;, &gt;, $, /, =, stb.) ne legyen benne!<br />Ezek alapján nevezd át kérlek!</p>
+	<p>A feltöltendő fájlokat hozd a következő formába <b>more_than_one_word.mp3, tukor_meg_lira.png, stb.</b>, vagyis az elnevezés során ügyelj arra, hogy <b>szóköz</b> illetve <b>ékezetes- és speciális karakter</b> (&amp;, @, #, &lt;, &gt;, $, /, =, stb.) ne legyen benne! Ezek alapján nevezd át kérlek!</p>
 <?php
 	require_once "fileupload/class.FlashUploader.php";
 	IAF_display_js();
@@ -15,3 +19,29 @@ if($_SESSION["privilege"] == "user") {
 }
 ?>
 </div>
+<section id="image_container">
+	<div class="holder">
+		<div class="scroll-pane">
+			<div class="text-content">
+				<h2>Feltöltött képek</h2>
+<?php
+	if ($handle = opendir('../picture')) {
+		echo '<ul class="image-list">';
+		while (false !== ($entry = readdir($handle))) {
+			if ($entry != "." && $entry != "..") {
+				echo '<li>
+						<a href="../picture/'.$entry.'" target="_blank">'.$entry.'</a>
+						<a href="?b=file&pic='.$entry.'" title="kép törlése">×</a>
+					</li>';
+			}
+		}
+		echo '</ul>';
+		closedir($handle);
+	} else {
+		echo '<p>Empty directory!</p>';
+	}
+?>
+			</div>
+		</div>
+	</div>
+</section>
